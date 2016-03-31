76526aee77a09f278eca9ccc4b4f765dd76cb27f
mikessh <mikhail.shugay@gmail.com>
2016-03-31 03:55:07 +0300

    #66: basic checks

--- src/BuildDatabase.groovy
+++ src/BuildDatabase.groovy
@@ -1,10 +1,3 @@
-/**
- * Created by mikesh on 3/30/16.
- */
-
-
-
-
 def COMPLEX_COLUMNS = [
         "cdr3.alpha",
         "v.alpha",
@@ -39,6 +32,29 @@ def COMPLEX_COLUMNS = [
         "meta.donor.MHC",
         "meta.donor.MHC.method",
         "meta.structure.id"
+],  SIGNATURE_COLS = [
+        "cdr3.alpha",
+        "v.alpha",
+        "j.alpha",
+        "cdr3.beta",
+        "v.beta",
+        "d.beta",
+        "j.beta",
+        "species",
+        "mhc.a",
+        "mhc.b",
+        "mhc.class",
+        "antigen.epitope",
+        "antigen.gene",
+        "antigen.species",
+        "reference.id",
+        "meta.study.id",
+        "meta.cell.subset",
+        "meta.subject.cohort",
+        "meta.subject.id",
+        "meta.replica.id",
+        "meta.clone.id",
+        "meta.tissue"
 ]
 
 def err = { String message ->
@@ -55,7 +71,7 @@ def checkHeader = { String[] header ->
     if (header.length != colSet.size())
         err("Duplicate columns found: $header")
 
-    def missingColumns = [COMPLEX_COLUMNS, METHOD_COLUMNS, METHOD_COLUMNS].flatten().findAll {
+    def missingColumns = [COMPLEX_COLUMNS, METHOD_COLUMNS, META_COLUMNS].flatten().findAll {
         !colSet.contains(it)
     }
 
@@ -63,28 +79,129 @@ def checkHeader = { String[] header ->
         err("The following columns are missing: $missingColumns")
 }
 
-def readChunk = { String fileName ->
-    def header
-    def rows = []
+class Table {
+    final String[] header
+    final List<Row> rows
+    final Map<String, Integer> indices = new HashMap<>()
 
-    new File(fileName).withReader { reader ->
+    Table(String[] header, List<String[]> rows) {
+        this.header = header
+        this.rows = rows.collect { new Row(it) }
+
+        header.eachWithIndex { String entry, int i ->
+            indices[entry] = i
+        }
+    }
+
+    class Row {
+        final String[] values
+
+        Row(String[] values) {
+            this.values = new String[values.length]
+
+            for (int i = 0; i < values.length; i++) {
+                this.values[i] = values[i].trim()
+            }
+        }
+
+        String getAt(String columnName) {
+            values[indices[columnName]]
+        }
+    }
+}
+
+
+def isAASeqValid = { String str ->
+    str =~ /^[ARNDCQEGHILKMFPSTWYV]+$/
+}
+
+def isMhcValid = { String str ->
+    !str.startsWith("HLA") || str =~ /^HLA-[A-Z]+\*\d{2}(:\d{2})?$/
+}
+
+def speciesList = ["homosapiens", "musmusculus",
+                   "rattusnorvegicus", "macacamulatta"]
+
+def validators = [
+        "cdr3.alpha"     : isAASeqValid,
+        "v.alpha"        : { it.startsWith("TRAV") },
+        "j.alpha"        : { it.startsWith("TRAJ") },
+        "cdr3.beta"      : isAASeqValid,
+        "v.beta"         : { it.startsWith("TRBV") },
+        "d.beta"         : { it.startsWith("TRBD") },
+        "j.beta"         : { it.startsWith("TRBJ") },
+        "species"        : { speciesList.any { species -> it.toLowerCase() == species } },
+        "mhc.a"          : isMhcValid,
+        "mhc.b"          : isMhcValid,
+        "mhc.class"      : { it == "MHCI" || it == "MHCII" },
+        "antigen.epitope": isAASeqValid,
+        "reference.id"   : {
+            it.startsWith("PMID:") || it.startsWith("doi:") || it.toLowerCase().contains("unpublished")
+        }
+]
+
+def readChunk = { File chunkFile ->
+    def header = null, rows = []
+
+    chunkFile.withReader { reader ->
         header = reader.readLine().split("\t")
         checkHeader(header)
 
         def line
         while (line = reader.readLine()) {
+            if (line.endsWith("\t")) {
+                line += " "
+            }
+
             def splitLine = line.split("\t")
+
             if (splitLine.length != header.length)
                 err("Row $splitLine has wrong number of columns")
+
             rows << splitLine
         }
     }
 
-    new Chunk(header, rows)
+    new Table(header, rows)
 }
 
-def chunks = new File("../chunks_new/").listFiles().toList()
+def chunkFiles = new File("../chunks_new/").listFiles().toList()
 
-if (chunks.empty)
+if (chunkFiles.empty)
     err("No database chunks to process")
 
+chunkFiles.each { chunkFile ->
+    def table = readChunk(chunkFile)
+
+    def chunkErrorMessages = [:]
+    def rowSignatures = new HashSet<String>()
+
+    table.rows.each { row ->
+        def rowSignature = SIGNATURE_COLS.collect { row[it] }.join(" | ")
+
+        if (rowSignatures.contains(rowSignature)) {
+            chunkErrorMessages[rowSignature] = "duplicate"
+        } else {
+            def rowErrorMessages = []
+
+            validators.each {
+                def cellValue = row[it.key]
+                if (cellValue.length() > 0 && !it.value(cellValue)) {
+                    rowErrorMessages << "bad." + it.key
+                }
+            }
+
+            if (rowErrorMessages) {
+                chunkErrorMessages[rowSignature] = rowErrorMessages.toString()
+            }
+        }
+    }
+
+    if (!chunkErrorMessages.empty) {
+        println([SIGNATURE_COLS, "error.message"].flatten().join(" | "))
+        chunkErrorMessages.each {
+            println(it.key + " | " + it.value)
+        }
+        err("There were errors processing $chunkFile.name")
+    }
+}
