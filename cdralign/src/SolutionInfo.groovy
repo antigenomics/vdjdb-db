5a75a659b4e0eef1a2cb9d4acdbfeb0899bf6995
mikessh <mikhail.shugay@gmail.com>
2016-06-25 16:15:39 +0300

    2prev

--- cdralign/src/SolutionInfo.groovy
+++ cdralign/src/SolutionInfo.groovy
@@ -8,46 +8,38 @@ import static com.milaboratory.core.mutations.Mutation.getTo;
 import static com.milaboratory.core.mutations.Mutation.isDeletion;
 import static com.milaboratory.core.mutations.Mutation.isInsertion;
 
-public class SolutionInfo {
-    private final LinearGapAlignmentScoring scoring;
-    private final double threshold;
+class SolutionInfo {
+    final LinearGapAlignmentScoring scoring
+    final double threshold
 
     SolutionInfo(LinearGapAlignmentScoring scoring, double threshold) {
-        this.scoring = scoring;
-        this.threshold = threshold;
+        this.scoring = scoring
+        this.threshold = threshold
     }
 
     public double computeScore(Alignment alignment) {
-        Sequence reference = alignment.getSequence1();
-        Mutations mutations = alignment.getAbsoluteMutations();
-        double score = 0;
+        Sequence reference = alignment.sequence1
+        Mutations mutations = alignment.absoluteMutations
+        double score = 0
 
         for (int i = 0; i < reference.size(); i++) {
-            byte aa = reference.codeAt(i);
-            score += scoring.getScore(aa, aa);
+            byte aa = reference.codeAt(i)
+            score += scoring.getScore(aa, aa)
         }
 
         for (int i = 0; i < mutations.size(); ++i) {
-            int mutation = mutations.getMutation(i);
+            int mutation = mutations.getMutation(i)
 
             if (isInsertion(mutation)) {
-                score += scoring.getGapPenalty();
+                score += scoring.getGapPenalty()
             } else {
-                byte from = getFrom(mutation);
-                score += isDeletion(mutation) ? scoring.getGapPenalty() :
-                        (scoring.getScore(from, getTo(mutation)));
-                score -= scoring.getScore(from, from);
+                byte from = getFrom(mutation)
+                score += isDeletion(mutation) ? scoring.gapPenalty :
+                        (scoring.getScore(from, getTo(mutation)))
+                score -= scoring.getScore(from, from)
             }
         }
 
-        return score;
-    }
-
-    public LinearGapAlignmentScoring getScoring() {
-        return scoring;
-    }
-
-    public double getThreshold() {
-        return threshold;
+        score
     }
 }
