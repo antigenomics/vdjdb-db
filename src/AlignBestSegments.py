e93d06c7e5db0cb64ed79c24edbe9a8fd391ab0c
Vadim Nazarov <vdn@mailbox.org>
2016-12-19 10:26:24 +0300

    draft for aa-nuc alignments

---
+++ src/AlignBestSegments.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+CODONS = 
+
+
+def align_nuc_to_aa(seq, gene):
+	pass
+
+
+def align_nuc_to_aa_rev(seq, gene):
+	pass
+
+
+def align_segments(seq, segments):
+	pass
+
+
+if __name__ == "__main__":
+	pass
