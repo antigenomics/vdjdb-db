ad2f63c7a46454eaad8f274c68219a6ce3c36f71
mikessh <mikhail.shugay@gmail.com>
2017-03-02 13:42:13 +0300

    WIP on server compatibility: change metadata (add autocomplete), remove D segm

--- src/SlimComplexAccumulator.groovy
+++ src/SlimComplexAccumulator.groovy
@@ -12,7 +12,7 @@ class SlimComplexAccumulator {
             "antigen.gene",
             "antigen.species"
     ],                        SUMMARY_COLS = ["complex.id",
-                                              "v.segm", "d.segm", "j.segm",
+                                              "v.segm", "j.segm",
                                               "v.end", "j.start",
                                               "mhc.a", "mhc.b", "mhc.class",
                                               "reference.id", "vdjdb.score"]
@@ -21,7 +21,6 @@ class SlimComplexAccumulator {
 
     final Set<String> complexId = new HashSet<>(),
                       vSegm = new HashSet<>(),
-                      dSegm = new HashSet<>(),
                       jSegm = new HashSet<>(),
                       mhcA = new HashSet<>(),
                       mhcB = new HashSet<>(),
@@ -36,7 +35,6 @@ class SlimComplexAccumulator {
     void append(List<String> splitLine, Map<String, Integer> colIdMap) {
         complexId.addAll(splitLine[colIdMap["complex.id"]].split("[,;]").findAll { it.length() > 0 && it != "0" })
         vSegm.addAll(splitLine[colIdMap["v.segm"]].split("[,;]").findAll { it.length() > 0 && it != "." })
-        dSegm.addAll(splitLine[colIdMap["d.segm"]].split("[,;]").findAll { it.length() > 0 && it != "." })
         jSegm.addAll(splitLine[colIdMap["j.segm"]].split("[,;]").findAll { it.length() > 0 && it != "." })
         mhcA.addAll(splitLine[colIdMap["mhc.a"]].split("[,;]").findAll { it.length() > 0 && it != "." })
         mhcB.addAll(splitLine[colIdMap["mhc.b"]].split("[,;]").findAll { it.length() > 0 && it != "." })
@@ -63,7 +61,6 @@ class SlimComplexAccumulator {
     String getSummary() {
         [complexId.empty ? "0" : complexId.join(","),
          vSegm.join(","),
-         dSegm.join(","),
          jSegm.join(","),
          vEnd,
          jStart,
