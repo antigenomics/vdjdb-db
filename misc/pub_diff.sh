6d765e8637c13953a4c88549b4ec7a05e488e82a
mikessh <mikhail.shugay@gmail.com>
2022-03-09 07:33:34 +0300

    Cleanup

--- misc/pub_diff.sh
+++
@@ -1,3 +0,0 @@
-cat ../database/vdjdb-2018-01-17/vdjdb.txt | cut -f13 | sort | uniq > pub_prev
-cat ../database/vdjdb-2018-05-05/vdjdb.txt | cut -f13 | sort | uniq > pub_new
-diff -a pub_prev pub_new
