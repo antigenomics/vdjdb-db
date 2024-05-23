9060072047b3bbb33c04b03b0d7c06d92b8199ec
mikessh <mikhail.shugay@gmail.com>
2024-05-23 16:07:36 +0300

    #364: fix

--- release_docker.sh
+++ release_docker.sh
@@ -1,2 +1,2 @@
-build --no-cache -t vdjdbdb . 2>&1 | tee database/docker_build.log
-run -v `pwd`/database/ vdjdbdb 2>&1 | tee database/docker_run.log
+docker build --no-cache -t vdjdbdb . 2>&1 | tee database/docker_build.log
+docker run -v `pwd`/database/ vdjdbdb 2>&1 | tee database/docker_run.log
