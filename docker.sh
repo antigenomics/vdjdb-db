f3949773541839f36cc18ff23f404d2baa9fe683
luppovdaniil <luppov.daniil17@gmail.com>
2024-11-15 23:13:14 +0300

    #395 git settings chage

--- docker.sh
+++ docker.sh
@@ -2,6 +2,9 @@
 mkdir -p vcs
 cd vcs
 echo $(pwd)
+git config --global http.postBuffer 500M
+git config --global http.maxRequestBuffer 100M
+git config --global core.compression 0
 git clone https://github.com/antigenomics/vdjdb-db vdjdb-db
 git clone https://github.com/antigenomics/vdjdb-motifs vdjdb-motifs
 git clone https://github.com/antigenomics/mirpy.git
