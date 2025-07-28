58b97862f61e5523c24a37e156380606263434ac
LuppovDaniil <luppov.dv@phystech.edu>
2025-07-28 22:38:05 +0300

    #443 scores fix

--- py_src/SlimDBGenerator.py
+++ py_src/SlimDBGenerator.py
@@ -12,7 +12,7 @@ COMPLEX_SLIM_ANNOT_COLS = [
 SUMMARY_COLS = ["complex.id",
                 "v.segm", "j.segm",
                 "mhc.a", "mhc.b", "mhc.class",
-                "reference.id", "vdjdb.score", "vdjdb.legacy.score"]
+                "reference.id", "vdjdb.score", "vdjdb.pgen.score"]
 
 
 def _aggregating_function(x):
@@ -27,7 +27,7 @@ def generate_slim_db(default_db: pd.DataFrame) -> None:
     slim_db = default_db[COMPLEX_SLIM_ANNOT_COLS + SUMMARY_COLS]
     slim_db['j.start'] = default_db['cdr3fix'].apply(lambda x: x['jStart'] if not x == '' else x)
     slim_db['v.end'] = default_db['cdr3fix'].apply(lambda x: x['vEnd'] if not x == '' else x)
-    legacy_scores_aggregates = slim_db.groupby(COMPLEX_SLIM_ANNOT_COLS)['vdjdb.legacy.score'].max().reset_index()['vdjdb.legacy.score']
+    scores_aggregates = slim_db.groupby(COMPLEX_SLIM_ANNOT_COLS)['vdjdb.score'].max().reset_index()['vdjdb.score']
     slim_db = slim_db.groupby(COMPLEX_SLIM_ANNOT_COLS).agg(_aggregating_function).reset_index()
-    slim_db['vdjdb.legacy.score'] = legacy_scores_aggregates
+    slim_db['vdjdb.score'] = scores_aggregates
     slim_db.set_index('gene').to_csv('../database/vdjdb.slim.txt', sep='\t', quotechar='"')
